9486e56c76afe713fb2183df22946db98d4fea40
patrickhaahr <patrickqhaahr@gmail.com>
2024-09-04 14:30:19 +0200

    ui update

--- script.js
+++ script.js
@@ -213,12 +213,11 @@ function updateOrderDetails() {
     const item = orderedItems[itemName];
     const listItem = document.createElement("li");
     listItem.innerHTML = `
-            <button class="modify-btn" onclick="addItem('${itemName}', ${item.price
-      })">+</button>
-            x<span>${item.quantity
-      }</span><button class="modify-btn" onclick="removeItem('${itemName}')">-</button>${itemName} ${item.price * item.quantity
-      }kr.
-        `;
+      <button class="modify-btn" onclick="removeItem('${itemName}')">-</button>
+      x<span>${item.quantity}</span>
+      ${itemName} ${item.price * item.quantity}kr.
+      <button class="modify-btn" onclick="addItem('${itemName}', ${item.price})">+</button>
+    `;
     orderList.appendChild(listItem);
   }
   updateCartButtons();
@@ -228,7 +227,6 @@ function updateOrderDetails() {
   document.getElementById(
     "cart-btn"
   ).textContent = `(${getOrderQuantity()}) Show order ${total.toFixed(2)}kr.`;
-  // If the cart is empty, hide the dropdown
   if (total === 0) {
     document.getElementById("shopping-cart-dropdown").style.display = "none";
   }
